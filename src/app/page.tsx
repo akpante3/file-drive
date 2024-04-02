aa476ced7fe34d8166b85949ac83075a87bfaa3b
Victor <career.victorobije@gmail.com>
2024-04-02 19:10:50 +0100

    clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { SignOutButton, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { SignInButton, useSession } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+  const session = useSession();
   return (
     <main>
-      <Button>hello</Button>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal"></SignInButton>
+      </SignedOut>
 
+      <Button>hello</Button>
     </main>
   );
 }
